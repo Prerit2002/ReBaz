f4603d7f9bb2fee535bcfcc9c85fac8ecf72c4b7
Prerit2002 <Prerit715@gmail.com>
2022-05-28 13:19:49 +0530

    pay functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import './App.css';
 import Web3 from 'web3/dist/web3.min.js'
-import contract from './contracts/hack.json'
+import hack from './contracts/hack.json'
 
 function App() {
   const [state, setState] = useState({ account: "" })
@@ -9,14 +9,16 @@ function App() {
     window.ethereum.request({ method: 'eth_requestAccounts' });
     const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
     const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
     const network = await web3.eth.net.getNetworkType()
     console.log("network:", network)
     setState((state) => ({ ...state, account: accounts[0] }))
-    const auctionContract = new web3.eth.Contract(contract, accounts[0])
+    const auctionContract = new web3.eth.Contract(hack.abi, accounts[0])
     setState(state => ({ ...state, auctionContract }))
   }
 
   const pay = (address , value) => {
+    alert("hi")
     state.auctionContract.methods.pay(address).send({ from: state.account, value  }).on('receipt' , (receipt) => {
       console.log(receipt)
     }).on('error' , (error) => {
@@ -35,6 +37,13 @@ function App() {
 
   return (
     <div className="App">
+      <button onClick={(e) => {
+        e.preventDefault()
+        pay(state.account, 100)
+      }
+        
+      }>pay</button>
+      helo
       
     </div>
   );
